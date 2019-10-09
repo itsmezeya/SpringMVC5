74f8e4a9252b8985144fa8f1a18873fa5c83e70e
Admin <Admin@Admin-PC>
2019-10-09 21:06:37 +0530

    springmvc5-form-handling

---
+++ springmvc5-form-handling/README.txt
@@ -0,0 +1,14 @@
+In this article, we will learn how to create and submit a simple form (signup form) in Spring MVC application using Spring MVC 5+, Maven build tool, JSP and Eclipse IDE or STS.
+In this example, we will use a Java-based configuration that is we configure the Spring DispatcherServlet and spring beans configuration using all Java Code (no XML).
+--------------------------------------
+Reference links:
+https://github.com/RameshMF/spring-mvc-tutorial/tree/master/springmvc5-form-handling
+https://www.youtube.com/watch?v=UbxMJn8o5ko
+https://www.javaguides.net/2018/10/spring-mvc-sign-up-form-handling.html
+----------------------------------
+
+------------------------------
+How to run application.
+1. Maven plugin already added to pom.xml
+2. Right click project--> Run as -->Maven Build -->(maven goal) tomcat7:run
+3. From browser hit -->  http://localhost:8080/spring-mvc/showSignUpForm
---
+++ springmvc5-form-handling/src/main/java/net/seedInfoTech/springmvc/form/config/AppConfig.java
@@ -0,0 +1,32 @@
+package net.seedInfoTech.springmvc.form.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "net.seedInfoTech.springmvc.form" })
+public class AppConfig implements WebMvcConfigurer{
+
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	// used to add static files like image,css
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/resources/"); 
+    }
+}
